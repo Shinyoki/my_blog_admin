26ea3e97401dc0665adba3eec5374e597be41c9b
senko <1641801056@qq.com>
2022-05-25 14:05:33 +0800

    完成部分菜单Menu视图的业务

--- src/views/menu/Menu.vue
+++ src/views/menu/Menu.vue
@@ -1,10 +1,267 @@
 <template>
-  <div></div>
+  <el-card class="main-card">
+    <div class="card-container-title"> {{ this.$route.name }}</div>
+<!--    操作-->
+    <div class="operation-container">
+<!--      增加菜单-->
+      <el-button
+        type="primary"
+        size="small"
+        icon="el-icon-plus"
+        @click="openModel"
+      >
+        新增菜单
+      </el-button>
+<!--      筛选-->
+      <div style="display: flex;margin-left: auto">
+        <el-input
+          v-model="keywords"
+          prefix-icon="el-icon-search"
+          size="small"
+          placeholder="菜单名"
+          @keyup.enter.native="listMenus"
+          />
+        <el-button
+          type="primary"
+          size="small"
+          icon="el-icon-search"
+          @click="listMenus"
+          >
+          搜索
+        </el-button>
+      </div>
+    </div>
+<!--    表格展示-->
+    <el-table
+      v-loading="loading"
+      :data="menuList"
+      row-key="id"
+      :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
+      >
+<!--      列：菜单名称-->
+      <el-table-column label="菜单名称" prop="name" align="center" width="160"/>
+<!--      列：菜单图标-->
+      <el-table-column label="图标" prop="icon" align="center" width="100">
+        <template slot-scope="scope">
+          <i :class="'iconfont ' + scope.row.icon"/>
+        </template>
+      </el-table-column>
+<!--      列：菜单排序-->
+      <el-table-column label="排序" prop="orderNum" align="center" width="100"/>
+<!--      列：访问路径-->
+      <el-table-column label="访问路径" prop="path"/>
+<!--      列：是否隐藏-->
+      <el-table-column label="隐藏" prop="isHidden" align="center" width="80">
+        <template slot-scope="scope">
+          <el-switch
+            v-model="scope.row.isHidden"
+            :active-value="1"
+            :inactive-value="0"
+            active-color="#13ce66"
+            inactive-color="#F4F4F5"
+            disabled
+            />
+        </template>
+      </el-table-column>
+<!--      列：创建时间-->
+      <el-table-column label="创建时间" prop="createTime" align="center" width="200">
+        <template slot-scope="scope">
+          <i class="el-icon-time" style="margin-right: 5px"/>
+          {{formatLocalDateTime(scope.row.createTime)}}
+        </template>
+      </el-table-column>
+<!--      列：操作-->
+      <el-table-column label="操作" align="center" width="200">
+        <template slot-scope="scope">
+          <el-button
+            type="text"
+            size="mini"
+            @click="addSubMenu(scope.row, 1)"
+            v-if="scope.row.children"
+            >
+            <i class="el-icon-plus" />新增
+          </el-button>
+          <el-button
+            type="text"
+            size="mini"
+            @click="editCurMenu(scope.row)"
+            >
+            <i class="el-icon-edit"/>修改
+          </el-button>
+          <el-popconfirm
+            title="确定要删除吗？"
+            style="margin-left: 10px"
+            @confirm="deleteMenu(scope.row.id)"
+            >
+            <el-button size="mini" type="text" slot="reference">
+             <i class="el-icon-delete"/> 删除
+            </el-button>
+          </el-popconfirm>
+        </template>
+      </el-table-column>
+    </el-table>
+<!--    dialog-->
+    <el-dialog
+      :visible.sync="showAddOrEditDialog"
+      width="30%"
+      top="12vh"
+      >
+      <div
+          class="dialog-title-container"
+          slot="title"
+          ref="menuTitle"
+          />
+      <el-form
+        size="medium"
+        :model="menuForm"
+        label-width="80px"
+        >
+<!--        菜单类型选项-->
+        <el-form-item label="菜单类型" v-if="showMenuTypeSelector">
+          <el-radio-group v-model="isCatalog">
+            <el-radio :label="true">目录</el-radio>
+            <el-radio :label="false">一级菜单</el-radio>
+          </el-radio-group>
+        </el-form-item>
+      </el-form>
+<!--TODO 剩余的修改页-->
+    </el-dialog>
+  </el-card>
 </template>
 
 <script>
 export default {
-  name: "MenuView"
+  name: "MenuView",
+  created() {
+    this.listMenus();
+  },
+  data() {
+    return {
+      loading: false,
+      showAddOrEditDialog: false,
+      isCatalog: true,
+      showMenuTypeSelector: true,
+      //data
+      menuList: [],
+      //request form
+      menuForm: {
+        id: null,
+        name: "",
+        icon: "",
+        component: "",
+        path: "",
+        orderNum: 1,
+        parentId: null,
+        isHidden: 0
+      },
+      //菜单名
+      keywords: "",
+    }
+  },
+  methods: {
+    //查询后台菜单
+    listMenus() {
+      let params = {
+        keywords: this.keywords
+      }
+      this.getRequest("/admin/menus", params).then(res => {
+        if (res.data.flag) {
+          this.menuList = res.data.data;
+
+          this.loading = false;
+        } else {
+          this.$notify.error({
+            title: '错误',
+            message: res.data.message
+          })
+        }
+      })
+    },
+    //新增菜单
+    addSubMenu(menu) {
+      this.openModel(menu, 1);
+    },
+    editCurMenu(menu) {
+      this.openModel(menu, 2)
+    },
+    openModel(menu, type) {
+      if (menu) {
+        //有则给该menu添加 子菜单
+        //关闭菜单类型选项的显示
+        this.showMenuTypeSelector = false;
+        this.isCatalog = false;
+
+        //操作类型
+        switch (type) {
+          case 1:
+            //添加子菜单
+            this.menuForm = {
+              id: null,
+              name: "",
+              icon: "",
+              component: "",
+              path: "",
+              orderNum: 1,
+              parentId: null,
+              isHidden: 0
+            }
+            this.$refs.menuTitle.innerHTML = "新增菜单";
+            this.menuForm.parentId = menu.id;
+            break;
+          case 2:
+            //修改
+            this.$refs.menuTitle.innerHTML = "修改菜单";
+            //深拷贝
+            this.menuForm = JSON.parse(JSON.stringify(menu))
+            break;
+        }
+      } else {
+        //无则对应添加 目录
+        this.$refs.menuTitle.innerHTML = "新增菜单";
+        //初始化表单数据
+        this.menuForm = {
+          id: null,
+          name: "",
+          icon: "",
+          //目录组件
+          component: "Layout",
+          path: "",
+          orderNum: 1,
+          parentId: null,
+          isHidden: 0
+        }
+
+        this.showAddOrEditDialog = true;
+      }
+    },
+    deleteMenu(id) {
+      this.deleteMenu("/admin/menus/" + id).then(res => {
+        if (res.data.flag) {
+          this.$notify.success({
+            title: "成功",
+            message: "删除成功"
+          });
+          this.listMenus();
+        } else {
+          this.$notify.error({
+            title: "失败",
+            message: res.data.message
+          });
+        }
+      })
+    }
+  },
+  computed: {
+    //格式化LocalDateTime
+    formatLocalDateTime() {
+      return function (dateStr) {
+        return this.$dayjs(dateStr).format("YYYY年MM月DD日 HH时mm分ss秒");
+      };
+    },
+  },
+  watch: {
+
+  }
 }
 </script>
 
--- src/views/message/Message.vue
+++ src/views/message/Message.vue
@@ -27,7 +27,120 @@
       >
         批量通过
       </el-button>
+<!--      筛选-->
+      <div style="display: flex; margin-left: auto">
+        <el-input
+          size="small"
+          placeholder="用户昵称"
+          v-model="keywords"
+          prefix-icon="el-icon-search"
+          @keyup.enter.native="doSearchMessages"
+          />
+        <el-button
+          type="primary"
+          size="small"
+          icon="el-icon-search"
+          style="margin-left: 24px"
+          @click="doSearchMessages"
+          >
+          搜索
+        </el-button>
+      </div>
     </div>
+<!--    表格展示-->
+    <el-table
+      border
+      v-loading="loading"
+      :data="messageList"
+      @selection-change="onSelectionChange"
+      >
+<!--      多选-->
+      <el-table-column type="selection" width="55"/>
+<!--      头像-->
+      <el-table-column label="头像" prop="avatar" align="center" width="150">
+        <template slot-scope="scope">
+          <img :src="scope.row.avatar" width="40" height="40"/>
+        </template>
+      </el-table-column>
+      <!--    发言人-->
+      <el-table-column label="发言人" prop="nickname" align="center" width="100"/>
+      <!--    留言内容-->
+      <el-table-column label="留言内容" prop="messageContent" align="center"/>
+      <!--    ip地址-->
+      <el-table-column label="IP地址" prop="ipAddress" align="center" width="100"/>
+      <!--    ip来源-->
+      <el-table-column label="IP来源" prop="ipSource" align="center" width="100">
+        <template slot-scope="scope">
+          <span v-if="scope.row.ipSource">{{ scope.row.ipSource }}</span>
+          <span v-else>未知</span>
+        </template>
+      </el-table-column>
+      <!--    审核状态-->
+      <el-table-column label="审核状态" prop="isReview" align="center" width="80">
+        <el-tag v-if="isReview == 0" type="warning">审核中</el-tag>
+        <el-tag v-else-if="isReview == 1" type="success">正常</el-tag>
+      </el-table-column>
+      <!--    留言时间-->
+      <el-table-column label="留言时间" prop="createTime" width="150" align="center">
+        <template slot-scope="scope">
+          <i class="el-icon-time" style="margin-right: 5px"/>
+          {{formatLocalDateTime(scope.row.createTime)}}
+        </template>
+      </el-table-column>
+<!--      操作-->
+      <el-table-column label="操作" width="160" align="center">
+        <template slot-scope="scope">
+          <el-button
+            type="success"
+            size="small"
+            v-if="scope.row.isReview == 0"
+            @click="doUpdateMessageIsReview(scope.row.id)"
+            >
+            通过
+          </el-button>
+          <el-popconfirm
+            style="margin-left: 5px"
+            title="确定要删除吗？"
+            @confirm="doDeleteMessage(scope.row.id)"
+            >
+            <el-button
+              slot="reference"
+              type="danger"
+              size="small"
+              >
+              删除
+            </el-button>
+          </el-popconfirm>
+        </template>
+      </el-table-column>
+    </el-table>
+<!--    分页-->
+    <el-pagination
+      background
+      class="pagination-container"
+      :current-page="current"
+      :page-size="size"
+      :page-sizes="[10, 20]"
+      :total="count"
+      @current-change="onCurrentChange"
+      @size-change="onSizeChange"
+      layout="total, sizes, prev, pager, next, jumper"
+      />
+<!--    dialog-->
+    <el-dialog
+      :visible.sync="showDeleteDialog"
+      width="30%"
+      >
+      <div class="dialog-title-container" slot="title">
+        <i class="el-icon-warning" style="color:#999"/>
+        提示
+      </div>
+      <div>是否要删除所选项？</div>
+      <div slot="footer">
+        <el-button @click="showDeleteDialog = false">取消</el-button>
+        <el-button @click="doDeleteSelectedMessaegs" type="danger">确定</el-button>
+      </div>
+    </el-dialog>
   </el-card>
 </template>
 
@@ -55,6 +168,11 @@ export default {
     }
   },
   methods: {
+    //查询
+    doSearchMessages() {
+      this.current = 1;
+      this.listMessages();
+    },
     //查询后台消息
     listMessages() {
       let params = {
@@ -80,12 +198,65 @@ export default {
       this.doUpdateMessageIsReview(this.messageIdList)
     },
     doUpdateMessageIsReview(id) {
-      let messageIdList = id.len == undefined ? [id] : [...id];
+      let messageIdList = id.length == undefined ? [id] : [...id];
       let params = {
         idList: messageIdList,
         isReview: 1
       }
-      //TODO UPDATE
+      this.putRequest("/admin/messages/review", params).then(res => {
+        if (res.data.flag) {
+          this.$notify.success({
+            title: '成功',
+            message: res.data.message
+          })
+
+          this.listMessages();
+        } else {
+          this.$notify.error({
+            title: "失败",
+            message: res.data.message
+          });
+        }
+      })
+    },
+    //删除
+    doDeleteSelectedMessaegs() {
+      this.doDeleteMessage(this.messageIdList);
+    },
+    doDeleteMessage(id) {
+      let messageIdList = id.length == undefined ? [id] : [...id];
+      this.deleteRequest("/admin/messages", messageIdList).then(res => {
+        if (res.data.flag) {
+          this.$notify.success({
+            title: "成功",
+            message: res.data.message
+          })
+
+          this.listMessages();
+        } else {
+          this.$notify.error({
+            title: "失败",
+            message: res.data.message
+          });
+        }
+
+        this.showDeleteDialog = false;
+      })
+    },
+    //多选
+    onSelectionChange(selectedMessages) {
+      this.messageIdList = [];
+      selectedMessages.forEach(message => {
+        this.messageIdList.push(message.id)
+      })
+    },
+    onCurrentChange(newCur) {
+      this.current = newCur;
+      this.listMessages();
+    },
+    onSizeChange(newSize) {
+      this.size = newSize;
+      this.listMessages();
     }
   },
   computed: {
@@ -93,6 +264,18 @@ export default {
       return function (isReview) {
         return this.isReview == isReview ? "active-review" : 'review';
       };
+    },
+    formatLocalDateTime() {
+      return function (dateStr) {
+        return this.$dayjs(dateStr).format("YYYY年MM月DD日 HH点mm分ss秒")
+      }
+    }
+
+  },
+  watch: {
+    isReview() {
+      this.current = 1;
+      this.listMessages();
     }
   }
 }
